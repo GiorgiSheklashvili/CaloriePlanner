fb126f17ccdcf7214ca540c39dfd1ace8c2dbf0c
gio <sheklashviligiorgi@gmail.com>
2017-05-04 03:42:14 +0400

    corrected parser

--- app/src/main/java/home/gio/calorieplanner/main/Main.java
+++ app/src/main/java/home/gio/calorieplanner/main/Main.java
@@ -23,7 +23,6 @@ import home.gio.calorieplanner.models.RetailChain;
 public class Main implements IMainModel {
     RetailChain retailChain = new RetailChain();
     List<Product> productList = new ArrayList<>();
-    Product tempProduct = new Product();
     private DatabaseReference databaseReference;
 
     @Override
@@ -41,64 +40,54 @@ public class Main implements IMainModel {
                     doc = Jsoup.connect("http://goodwilldelivery.ge/category.aspx?id=4").get();//sakvebi produqti
                     Elements subLinks = doc.select("div[class=\"sub_links\"]");
                     Elements categories = doc.select("div[id=\"ProductMenu\"]").select("a[class=\"sub_menu\"]");
-                    Elements hrefs = subLinks.select("a[href]");
-                    int categoriesIndex = 0;
-//                    for (int subMenuIndex = 0; subMenuIndex < hrefs.size(); subMenuIndex++) {
-//
-//                        if (subMenuIndex == 12 || subMenuIndex == 17 || subMenuIndex == 30 || subMenuIndex == 39 || subMenuIndex == 45 || subMenuIndex == 47 || subMenuIndex == 49 || subMenuIndex == 57 || subMenuIndex == 60 || subMenuIndex == 63) {
-//                            categoriesIndex++;
-//                        }
-                    StringBuilder link = new StringBuilder(baseURL + "category.aspx?id=127");
-//                        String link = "http://goodwilldelivery.ge/category.aspx?id=107";
-                    doc = Jsoup.connect(link.toString()).get();
-                    Elements pages = doc.select("div[class=\"pager\"]");
-                    int pagesQuantity = pages.get(0).text().length();
-                    for (int i = 1; i <= pagesQuantity; i++) {
-                        tempString = "&page=" + String.valueOf(i);
-                        link.append(tempString);
-                        doc = Jsoup.connect(link.toString()).get();
-                        Elements links = doc.select("div[class=\"ProductContainer\"]");
-                        for (int j = 0; j < links.size(); j++) {//getting picture and name of product
-                            tempProduct.setCategory(categories.get(0).text());//category name
-                            tempProduct.setSubMenu(hrefs.get(1).text());//submenu name
-                            tempProduct.setImageURL(links.get(j).select("a[href]").select("img").attr("src"));//picture
-                            tempProduct.setName(links.get(j).select("a[href]").text());//name
-                            tempProduct.setPrice(links.get(j).select("strong").text());//price
-                            docDetails = Jsoup.connect("http://goodwilldelivery.ge/" + links.get(j).select("a").attr("href")).get();//details link
-                            detailsLink = docDetails.select("div[class=\"AdvancedControls\"]");
-                            tempProduct.setDetails(detailsLink.text());//Product Details
-                            tempProduct.setFat(Integer.parseInt(getFat(getNutritionFacts(detailsLink.text()))));
-                            tempProduct.setProtein(Integer.parseInt(getProtein(getNutritionFacts(detailsLink.text()))));
-                            tempProduct.setCarbohydrates(Integer.parseInt(getCarbs(getNutritionFacts(detailsLink.text()))));
-                            tempProduct.setCalories(Integer.parseInt(getCalories(getNutritionFacts(detailsLink.text()))));
-                            productList.add(tempProduct);
-                        }
-                        int k = link.indexOf(tempString);
-                        if (k != -1) {
-                            link.delete(k, k + tempString.length());
-                        }
-                    }
-                    Log.d("gamoagwia", "link.size foridan");
+                        Elements children = subLinks.get(10).children();
+                        for (int subMenuIndex = 0; subMenuIndex < children.size(); subMenuIndex++) {
+                            Elements hrefs = children.select("a[href]");
+                            StringBuilder link = new StringBuilder(baseURL + "category.aspx?id=171");//hrefs.get(subMenuIndex).select("a[href]").attr("href")
+                            doc = Jsoup.connect(link.toString()).get();
+                            Elements pages = doc.select("div[class=\"pager\"]");
+                            int pagesQuantity = pages.get(0).text().length();
+                            for (int i = 1; i <= pagesQuantity; i++) {
+                                tempString = "&page=" + String.valueOf(i);
+                                link.append(tempString);
+                                doc = Jsoup.connect(link.toString()).get();
+                                Elements links = doc.select("div[class=\"ProductContainer\"]");
+                                for (int j = 0; j < links.size(); j++) {//getting picture and name of product
+                                    Product tempProduct = new Product();
+                                    tempProduct.setCategory(categories.get(10).text());//category name
+                                    tempProduct.setSubMenu(hrefs.get(2).text());//submenu name
+                                    tempProduct.setImageURL(links.get(j).select("a[href]").select("img").attr("src"));//picture
+                                    tempProduct.setName(links.get(j).select("a[href]").text());//name
+                                    tempProduct.setPrice(links.get(j).select("strong").text());//price
+                                    docDetails = Jsoup.connect("http://goodwilldelivery.ge/" + links.get(j).select("a").attr("href")).get();//details link
+                                    detailsLink = docDetails.select("div[class=\"AdvancedControls\"]");
+                                    tempProduct.setDetails(detailsLink.text());//Product Details
+                                    tempProduct.setFat(Integer.parseInt(getFat(getNutritionFacts(detailsLink.text()))));
+                                    tempProduct.setProtein(Integer.parseInt(getProtein(getNutritionFacts(detailsLink.text()))));
+                                    tempProduct.setCarbohydrates(Integer.parseInt(getCarbs(getNutritionFacts(detailsLink.text()))));
+                                    tempProduct.setCalories(Integer.parseInt(getCalories(getNutritionFacts(detailsLink.text()))));
+
+
+                                    productList.add(tempProduct);
+                                }
+                                int k = link.indexOf(tempString);
+                                if (k != -1) {
+                                    link.delete(k, k + tempString.length());
+                                }
+                            }
+                            databaseReference = FirebaseDatabase.getInstance().getReference();
+                            databaseReference.child("Goodwill").child(productList.get(productList.size() - 1).getCategory().replaceAll("[#$.\\]\\[]", " ")).child(productList.get(productList.size() - 1).getSubMenu().replaceAll("[#$.\\]\\[]", " ")).setValue(productList);
+                            productList.clear();
 
 //                    }
-                    databaseReference = FirebaseDatabase.getInstance().getReference();
-                    databaseReference.child("Goodwill").child(productList.get(productList.size() - 1).getCategory().replaceAll("[#$.\\]\\[]","")).child(productList.get(productList.size() - 1).getSubMenu().replaceAll("[#$.\\]\\[]","")).setValue(productList);
-//                    for (Product product : productList) {
-//                        databaseReference.child("Goodwill").setValue(product);
+
+                        }
 //                    }
                 } catch (Exception ex) {
                     Log.d("network", "error in parsing" + ex.toString());
                 }
             }
         }).start();
-
-        App app = (App) context.getApplicationContext();
-//        databaseReference = FirebaseDatabase.getInstance().getReference();
-//        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-//        inputMethodManager.hideSoftInputFromWindow(((Activity) context).getCurrentFocus().getWindowToken(), 0);
-//        for (Product product : productList) {
-//            databaseReference.child("Goodwill").setValue(product);
-//        }
     }
 
     private String getFat(String details) {
@@ -129,7 +118,7 @@ public class Main implements IMainModel {
         if (index == -1)
             return "0";
         Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(details.substring(index, index + 15));
+        Matcher matcher = pattern.matcher(details.substring(index, index + 17));
         matcher.find();
 
         return matcher.group();
@@ -140,7 +129,7 @@ public class Main implements IMainModel {
         if (index == -1)
             return "0";
         Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(details.substring(index, index + 35));
+        Matcher matcher = pattern.matcher(details.substring(index, index + 29));
         matcher.find();
         return matcher.group();
     }
